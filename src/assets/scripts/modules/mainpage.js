1b040b4550c601cbc5baaf4c32cb7bcb79fce557
Egor Levchenko <antytoto@gmail.com>
2020-10-03 15:49:35 +0300

    created mainpage togglers

---
+++ src/assets/scripts/modules/mainpage.js
@@ -0,0 +1,16 @@
+const mainpageMain = document.querySelector('.mainpage__main');
+
+if (document.contains(mainpageMain)) {
+  const togglerHTML = mainpageMain.querySelector('.mainpage__search-toggler_html');
+  const togglerCSS = mainpageMain.querySelector('.mainpage__search-toggler_css');
+  const togglerJS = mainpageMain.querySelector('.mainpage__search-toggler_js');
+
+  function changeToggler () {
+    this.classList.toggle('mainpage__search-toggler_active');
+  }
+
+  togglerHTML.addEventListener('click', changeToggler);
+  togglerCSS.addEventListener('click', changeToggler);
+  togglerJS.addEventListener('click', changeToggler);
+}
+
