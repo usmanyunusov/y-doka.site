baa5a47979844f41546cb071a106157aad34e19a
Alena Batitskaya <solar.rust@gmail.com>
2020-10-08 12:26:19 +0300

    Fix

--- src/assets/scripts/modules/search.js
+++ src/assets/scripts/modules/search.js
@@ -18,8 +18,7 @@ const buildSuggestionItem = (caption, url) => {
   } else {
     item = document.createElement('a')
     item.href = url
-    // item.rel = 'noopener'
-    // item.setAttribute("role", "option")
+    item.setAttribute("role", "option")
   }
 
   item.classList.add("search__suggestions__item")
@@ -100,41 +99,28 @@ searchInput.addEventListener('keydown', (event) => {
       if (x) x = x.getElementsByTagName("a");
 
       if (event.key === "ArrowDown") {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
         currentFocus++;
-        /*and and make the current item more visible:*/
         addActive(x);
-      } else if (event.key === "ArrowUp") { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
+      } else if (event.key === "ArrowUp") {
         currentFocus--;
-        /*and and make the current item more visible:*/
         addActive(x);
       } else if (event.key === "Enter" || event.keyCode === 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
         event.preventDefault();
 
-        // window.location.href = window.location.href + "bla-bla";
         if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
           if (x) x[currentFocus].click();
         }
       }
 
       function addActive(x) {
-        /*a function to classify an item as "active":*/
         if (!x) return false;
-        /*start by removing the "active" class on all items:*/
         removeActive(x);
         if (currentFocus >= x.length) currentFocus = 0;
         if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
         x[currentFocus].classList.add("autocomplete-active");
       }
 
       function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
         for (let i = 0; i < x.length; i++) {
           x[i].classList.remove("autocomplete-active");
         }
