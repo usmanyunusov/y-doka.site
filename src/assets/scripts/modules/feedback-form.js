25157aa0dd1d6e0448db1880da91e3002de84d36
Nikolai Lopin <code@lopin.me>
2020-10-13 18:16:09 +0200

    feat: test AJAX feedback form submission

--- src/assets/scripts/modules/feedback-form.js
+++ src/assets/scripts/modules/feedback-form.js
@@ -2,6 +2,29 @@ const optionalFeedbackCells = document.querySelectorAll('.feedback-form__optiona
 const textArea = document.querySelector('.feedback-form__textarea')
 const submitButton = document.querySelector('.feedback-form__actionbar button[type=submit]')
 
+const form = document.querySelector('form.feedback-form')
+form && form.addEventListener('submit', function(event) {
+	event.preventDefault()
+	const formData = new FormData(this)
+	const payload = {}
+	for (let [field, value] of formData.entries()) {
+		payload[field] = value
+	}
+
+	fetch(this.getAttribute('action'), {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(payload)
+	}).then(res => {
+		console.log(res)
+		alert('all good')
+	}).catch(err => {
+		alert('all bad')
+	})
+})
+
 const usefulContentRadioButton = document.getElementById('feedack-useful')
 usefulContentRadioButton && usefulContentRadioButton.addEventListener('change', () => {
 	optionalFeedbackCells.forEach(cell => {
@@ -20,6 +43,7 @@ notUsefulContentRadioButton && notUsefulContentRadioButton.addEventListener('cha
 
 optionalFeedbackCells && optionalFeedbackCells.forEach(cell => {
 	cell.addEventListener('transitionend', () => {
+		debugger
 		if (cell.classList.has('feedback-form__cell')) {
 			window.scrollTo({
 				top: submitButton.offsetTop,
